f4122cae72201ddcc1f0e66681f7903663538f87
Will Lowenkamp <lowenkamp97@gmail.com>
2023-08-22 20:29:30 -0400

    still working on front end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Main";
 import Users from "./Users";
 import UserProfile from "./UserProfile";
@@ -8,13 +8,21 @@ import SignUp from "./SignUp";
 import Navbar from "./Navbar";
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]); // State to store search results
 
   const handleSearch = (searchQuery) => {
-    
-  }
+    fetch(`http://127.0.0.1:5555/api/albums?q=${searchQuery}`)
+      .then((response) => response.json())
+      .then((data) => setSearchResults(data))
+      .catch((error) => {
+        console.error('Error fetching search results:', error);
+        setSearchResults([]);
+      });
+  };
+
   return (
     <Router>
-      <Navbar />
+      <Navbar handleSearch={handleSearch} /> {/* Pass handleSearch to Navbar */}
       <Switch>
         <Route exact path="/">
           <Main />
@@ -37,4 +45,5 @@ const App = () => {
 };
 
 export default App;
+
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from "react-router-dom"
 
-const Login = () => {
+function Login({loginStatus, handleLogin}) {
+
+  const history = useHistory();
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [loginError, setLoginError] = useState(null)
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value)
+  };
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value)
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+      });
+      if (response.ok) {
+        const data = await response.json();
+        if (data.message === "Login successful") {
+          console.log("Login successful");
+          setLoginError(null);
+          handleLogin(data);
+          history.push(`/profile/${data.user_id}`);
+        } else {
+          console.log("Login failed");
+          setLoginError("Invalid username or password");
+        }
+      } else {
+        console.log("HTTP request failed with status: " + response.status);
+        setLoginError("Invalid username or password");
+      }
+    } catch (error) {
+      console.error("Error logging in: ", error);
+      setLoginError("An error occurred while logging in");
+    }
+}
+  
+
   return (
     <div>
-      <h1>Log In</h1>
+      <div className="login-container">
+        {loginError ? (
+          <h2 className="login-error">{loginError}</h2>
+        ) : null}
+        <h1 className='log-in-header'>Welcome Back</h1>
+        <input type="text" placeholder="Username" onChange={handleUsernameChange}/>
+        <input type="password" placeholder="Password" onChange={handlePasswordChange}/>
+        <button className="login-button" onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
-};
+}
 
 export default Login;
